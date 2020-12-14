978f511d1c2b1d1af51178ee5aa7687e23d2cce5
Amit Suneja <amitsuneja.devops@gmail.com>
2020-12-14 06:50:55 +0530

    Update ForLoopChallangeVideo65.java

--- ForLoopChallangeVideo65.java
+++ ForLoopChallangeVideo65.java
@@ -11,19 +11,67 @@ public class Challenge {
                  available after for loop ends. Note even when condition met the value is still incremented
                  and  printed outside loop.
          */
-        for (int i=0; i<=5; i++){
+        for (int i=0; i<=5; i++){  // we initialize variable with begning of for loop
+            // i variable will be destroy the movement control comes out of for loop
+            // which means we cant use it outside loop.
+
             System.out.println(i);
         }
-        // System.out.println(i);
 
-        int j;
+        int j; // we initialized j before staring the loop.This will be available after loop is finished.
         for (j=0; j<=5; j++){
             System.out.println(j);
         }
-         System.out.println(j);
+        System.out.println(j);
+        System.out.println("****************************");
+
+
+
+        int totalSumOfNumbers = 0;
+        int countOfNumbers = 0;
+        for (int i=1; i<=1000; i++){
+            if (( i % 3 == 0) && ( i % 5 == 0 )){  // test if number is divisble by 3 and 5
+                System.out.println(i);
+                totalSumOfNumbers += i;
+                countOfNumbers += 1;
+            }
+            if (countOfNumbers == 5){
+                break;      // this statement will break the for loop.
+            }
+
+        }
+        System.out.println("totalSumOfNumbers =" + totalSumOfNumbers);
+        System.out.println("****************************");
+
+/*
+While loop
+ */
+
+        int count = 0; // Note : count has to be defined before the start of loop.
+        while (count <=5){
+            System.out.println("i=" + count);
+            count ++;
+        }
+        System.out.println("****************************");
+
+        count = 0;
+        while (true){  // Note : Most commonly used loop, i used last time in unix when copying
+            if (count >= 6 ){    // snapshot VDH to blob storage and sleep the process while copy in progess
+                break;
+            }
+            System.out.println("i=" + count);
+            count ++;
+        }
+        System.out.println("****************************");
+        count = 9;
+        do {
+            System.out.println("i=" + count);
+            count ++;
+        }while (count <=5); // Note : we initialized count at 9 so condition as false , But still thil loop
+        // will run once irrespective of condition is false.
+        System.out.println("****************************");
+
 
     }
-
-
 
 }
