a2a61cb2e6f752be7aa3879ddb7df9a196e164f1
Amit Suneja <amitsuneja.devops@gmail.com>
2020-12-22 14:11:55 +0530

    Create tip.java

---
+++ Video105ArrayList/tip.java
@@ -0,0 +1,45 @@
+/*
+Tip:
+ArrayList will only contain object . You cant create 
+
+ArrayList<int> numbersInArrayList = new ArrayList<int>
+  
+This is incorrect. 
+
+Instead create a class which have int variable and then create object  of call using its consutructor and store it in ArrayList.
+*/
+
+
+
+
+
+Public class myIntClass {
+  
+  
+  
+  // declaring class variables
+  Private int myIntVar;
+  
+  
+  
+  // Creating constuctor
+  Public myIntClass(int myIntVar){
+         this.myIntVar = myIntVar;
+  } // constuctor end here
+  
+  
+  // create setter
+  Public setmyIntVar(int myIntVar){
+        this.myIntVar = myIntVar
+  } // setter end here
+  
+  
+  // create Getter
+  Public getmyIntVar(){
+      return myIntVar
+  }
+ 
+  
+
+
+}
