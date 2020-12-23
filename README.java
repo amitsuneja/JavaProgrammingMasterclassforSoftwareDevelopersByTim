4c6e18e0fa05f750d05b66f13264125b5326b42d
Amit Suneja <amitsuneja.devops@gmail.com>
2020-12-23 13:32:05 +0530

    Update README.java

--- README.java
+++ README.java
@@ -37,7 +37,52 @@ ArrayList:
 5. Java ArrayList class is non synchronized.
 6. Java ArrayList allows random access because array works at the index basis.
 7. In ArrayList, manipulation is little bit slower than the LinkedList in Java because a lot of shifting needs to occur if any element is removed from the array list.
+8. ArrayList can not be used for primitive types, like int, char, etc. We need a wrapper class for such cases.
 
 */
 
 ArrayList<String> list=new ArrayList<String>();//creating new generic arraylist  
+
+
+
+
+
+public class Arraylist { 
+    public static void main(String[] args) 
+    { 
+        int n = 5; 
+  
+        // Here al is an array of arraylist having 
+        // n number of rows.The number of columns on 
+        // each row depends on the user. 
+        // al[i].size() will give the size of the 
+        // i'th row 
+        ArrayList<Integer>[] al = new ArrayList[n]; 
+  
+        // initializing 
+        for (int i = 0; i < n; i++) { 
+            al[i] = new ArrayList<Integer>(); 
+        } 
+  
+        // We can add any number of columns to each 
+        // rows as per our wish 
+        al[0].add(1); 
+        al[0].add(2); 
+        al[1].add(5); 
+        al[2].add(10); 
+        al[2].add(20); 
+        al[2].add(30); 
+        al[3].add(56); 
+        al[4].add(34); 
+        al[4].add(67); 
+        al[4].add(89); 
+        al[4].add(12); 
+  
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < al[i].size(); j++) { 
+                System.out.print(al[i].get(j) + " "); 
+            } 
+            System.out.println(); 
+        } 
+    } 
+} 
